531bdf4e0947309f16445ba680f9c86b49df2e0a
yoshimitendo <vk6sgysw7p@privaterelay.appleid.com>
2026-09-20 14:48:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function init() {
         1000
     );
     
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     
     renderer.setSize(window.innerWidth, window.innerHeight);
     
